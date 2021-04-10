939ee00449633aac86783a836c2ac3ea4d015eed
salahdin-ahmed <salah2112004@gmail.com>
2021-04-10 23:32:47 +0200

    Apply newterm patch

--- config.h
+++ config.h
@@ -279,6 +279,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
 	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
+	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 };
 
